dad3344265f41895b8408c130c3a21205575d663
Sagnik Datta <sagnik.datta@citrix.com>
2014-03-08 10:57:24 +0000

    Adding wordEngine

--- words.py
+++ words.py
@@ -1,35 +1,67 @@
 """Modules from extracting words from Membean"""
 
 from xml.dom import minidom
-import urllib
+from urllib import urlopen
+import nltk
 
-class MembeanResource(object):
+class WordDatabase():
     
     def __init__(self):
-        membeanWordsUrl = 'http://membean.com/products/GRE/plans/Voyage/\
+        self.membeanWordsUrl = 'http://membean.com/products/GRE/plans/Voyage/\
                            words.xml'
-        wordDict = {}
+        self.wordDict = {}
+        self.createWordDict()
+
     def createWordDict(self):    
-        site = urllib.urlopen(self.membeanWordsUrl)
-        dom = minidom.parse(site)                i
+        site = urlopen(self.membeanWordsUrl)
+        dom = minidom.parse(site)               
         words_tag = dom.getElementsByTagName('words')[0]
         letters = words_tag.getElementsByTagName('letter')
         for letter in letters:
             letter_key = str(letter._attrs['id'].value)
-            #wordDict[letter_key]={}
+            #self.wordDict[letter_key]={}
             levels = letter.getElementsByTagName('level')
             for level in levels:
                 level_key = str(level._attrs['difficulty'].value)
-                #wordDict[letter_key][level_key]={}
+                #self.wordDict[letter_key][level_key]={}
                 words = level.getElementsByTagName('word')
-                #wordDict[letter_key][level_key]=[]
+                #self.wordDict[letter_key][level_key]=[]
                 for word in words:
                     word_key = str(word.attributes.values()[0].value)
-                    #wordDict[letter_key][level_key].append(word_key) 
-                    wordDict[word_key]=(letter_key,level_key)
-         return wordDict
-    
-    def createWordDict(self):
-             
-    def mapLevelsToWords(self):
+                    #self.wordDict[letter_key][level_key].append(word_key) 
+                    self.wordDict[word_key]=(letter_key,level_key)
+        return self.wordDict
+
+class urlProcessor():
+    """Input url and returns a string of textual material"""
+    def __init__(self, url):    
+        self.wordDict = self.WordDatabase()
+        self.url = url
+        self.raw = self.makeUrlReady(self.url)
+
+    def makeUrlReady(self, url):
+        html = urlopen(url).read()
+        raw = nltk.clean_html(html)
+        return raw
+
+class wordEngine(urlProcessor)    
+    """input is the raw material and returns sentences using words"""
+    def __init__(self, raw):
+        if not self.raw:
+            self.raw = raw       
+
+    def get_Words(self):
+        wordsFound = []
+        for word in self.wordDict.keys():
+            if word in self.raw.split(" "):
+                wordsFound.append(word)
+        print wordsFound
+        return wordsFound
+
+    def get_sentences_from_word(self, word):
+        return re.findall(r'(.+%s.+)\.'%word, self.raw)[0].strip()
+
+
+
+        
         
