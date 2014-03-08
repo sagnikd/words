1057671c97bcf6ab1bdcd4b3408aa6bc49779727
Sagnik Datta <sagnik.datta@citrix.com>
2014-03-08 20:30:04 +0000

    Finish basic code

--- words.py
+++ words.py
@@ -1,67 +1,70 @@
+#!/usr/bin/env python
 """Modules from extracting words from Membean"""
-
 from xml.dom import minidom
 from urllib import urlopen
-import nltk
+import nltk, re
+from argparse import ArgumentParser
 
-class WordDatabase():
-    
-    def __init__(self):
-        self.membeanWordsUrl = 'http://membean.com/products/GRE/plans/Voyage/\
-                           words.xml'
-        self.wordDict = {}
-        self.createWordDict()
-
-    def createWordDict(self):    
-        site = urlopen(self.membeanWordsUrl)
-        dom = minidom.parse(site)               
-        words_tag = dom.getElementsByTagName('words')[0]
-        letters = words_tag.getElementsByTagName('letter')
-        for letter in letters:
-            letter_key = str(letter._attrs['id'].value)
-            #self.wordDict[letter_key]={}
-            levels = letter.getElementsByTagName('level')
-            for level in levels:
-                level_key = str(level._attrs['difficulty'].value)
-                #self.wordDict[letter_key][level_key]={}
-                words = level.getElementsByTagName('word')
-                #self.wordDict[letter_key][level_key]=[]
-                for word in words:
-                    word_key = str(word.attributes.values()[0].value)
-                    #self.wordDict[letter_key][level_key].append(word_key) 
-                    self.wordDict[word_key]=(letter_key,level_key)
-        return self.wordDict
-
-class urlProcessor():
+def urlProcessor(options):
     """Input url and returns a string of textual material"""
-    def __init__(self, url):    
-        self.wordDict = self.WordDatabase()
-        self.url = url
-        self.raw = self.makeUrlReady(self.url)
+    url = options.url
+    html = urlopen(url).read()
+    raw = nltk.clean_html(html)
+    return raw
 
-    def makeUrlReady(self, url):
-        html = urlopen(url).read()
-        raw = nltk.clean_html(html)
-        return raw
-
-class wordEngine(urlProcessor)    
+class wordEngine(): 
     """input is the raw material and returns sentences using words"""
-    def __init__(self, raw):
-        if not self.raw:
-            self.raw = raw       
-
+    def __init__(self, wordDict, raw):
+        self.raw = raw       
+        self.wordDict = wordDict
+        for word in self.get_Words():
+            print "%s : %s\n\n" % (word, self.get_sentences_from_word(word))
+         
     def get_Words(self):
         wordsFound = []
         for word in self.wordDict.keys():
             if word in self.raw.split(" "):
                 wordsFound.append(word)
-        print wordsFound
         return wordsFound
 
     def get_sentences_from_word(self, word):
-        return re.findall(r'(.+%s.+)\.'%word, self.raw)[0].strip()
+        return re.findall(r'([^\.]+%s[^\.]+)'%word, self.raw)[0].strip()
 
+class wordDatabase(object):
+    def __init__(self, wordDict):
+        if not wordDict:
+            self.membeanWordsUrl = \
+                'http://membean.com/products/GRE/plans/Voyage/words.xml'
+            self.wordDict = {}
+            self.wordDict = self.createWordDict()
+        else:
+            self.wordDict = wordDict
 
+    def createWordDict(self):
+        self.site = urlopen(self.membeanWordsUrl)
+        self.dom = minidom.parse(self.site)
+        self.words_tag = self.dom.getElementsByTagName('words')[0]
+        letters = self.words_tag.getElementsByTagName('letter')
+        for letter in letters:
+            letter_key = str(letter._attrs['id'].value)
+            levels = letter.getElementsByTagName('level')
+            for level in levels:
+                level_key = str(level._attrs['difficulty'].value)
+                words = level.getElementsByTagName('word')
+                for word in words:
+                    word_key = str(word.attributes.values()[0].value)
+                    self.wordDict[word_key]=(letter_key,level_key)
+        return self.wordDict
 
-        
-        
+def main():
+    """Entry Point"""
+    argParser = ArgumentParser()
+    argParser.add_argument("-u", "--url", dest="url", required=True,
+                            help="Enter url where you want to search words")
+    cmdargs = argParser.parse_args() 
+    wordDict = wordDatabase(None).wordDict # None implies to use Membean Database itself
+    raw = urlProcessor(cmdargs)
+    wordEngine(wordDict, raw)
+
+if __name__ == "__main__":
+    main()
